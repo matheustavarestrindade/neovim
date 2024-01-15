259f1f3e2176711457183970d94c1f2907427bd4
Matheus Tavares <matheus.trindade@pravaler.com.br>
2024-01-15 13:55:35 -0300

    Adicionando remap

--- lua/matheus/remap.lua
+++ lua/matheus/remap.lua
@@ -47,3 +47,13 @@ vim.keymap.set("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
+
+-- Move between views
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- Open vertical split
+vim.keymap.set("n", "<leader>v", "<C-w>v")
+
